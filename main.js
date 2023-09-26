19b316c7bc7999536a4434741291da9512644959
ThanhHM2 <thanhhm2@vng.com.vn>
2023-09-26 21:16:52 +0700

    remove eslint and using prettier

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { BVHLoader } from "three/addons/loaders/BVHLoader.js";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { BVHLoader } from 'three/addons/loaders/BVHLoader.js';
 
 const clock = new THREE.Clock();
 
@@ -12,52 +12,58 @@ init();
 animate();
 
 const loader = new BVHLoader();
-loader.load("models/bvh/001_Neutral_0_mirror_x_1_0.bvh", function (result) {
-  const skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0]);
+loader.load('models/bvh/001_Neutral_0_mirror_x_1_0.bvh', function (result) {
+    const skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0]);
 
-  scene.add(result.skeleton.bones[0]);
-  scene.add(skeletonHelper);
+    scene.add(result.skeleton.bones[0]);
+    scene.add(skeletonHelper);
 
-  // play animation
-  mixer = new THREE.AnimationMixer(result.skeleton.bones[0]);
-  mixer.clipAction(result.clip).play();
+    // play animation
+    mixer = new THREE.AnimationMixer(result.skeleton.bones[0]);
+    mixer.clipAction(result.clip).play();
 });
 
 function init() {
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.set(0, 200, 300);
+    camera = new THREE.PerspectiveCamera(
+        60,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+    camera.position.set(0, 200, 300);
 
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xeeeeee);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xeeeeee);
 
-  scene.add(new THREE.GridHelper(400, 10));
+    scene.add(new THREE.GridHelper(400, 10));
 
-  // renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+    // renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 300;
-  controls.maxDistance = 700;
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 300;
+    controls.maxDistance = 700;
 
-  window.addEventListener("resize", onWindowResize);
+    window.addEventListener('resize', onWindowResize);
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-  const delta = clock.getDelta();
+    const delta = clock.getDelta();
 
-  if (mixer) mixer.update(delta);
+    if (mixer) mixer.update(delta);
 
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
+
